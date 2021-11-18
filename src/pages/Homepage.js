7e4ee0ab2d1a03aa588f537bcae2caeb67bc5646
Gold-Dev726 <goldendev726@gmail.com>
2021-11-18 08:18:31 -0500

    update

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 // material
 import {
   Box,
@@ -49,7 +50,74 @@ const TEAMS = [
   },
 ];
 
+const TEAM_INFOS = [
+  {
+    number: 1,
+    name: "FIGHTER",
+    description1:
+      "In the distant past a warrior named Kuang Tu Nha helped the Chou dynasty defeat Tru, a dangerous warrior that had threatened everything. The legend of this battle has drawn hundreds of the greatest Taoists to study his hand to hand style, hoping to become as great as the legendary warrior.",
+    description2:
+      "Fighters are warriors who focus on close combat. They use their fists to make rapid attacks to wear down their enemies.",
+  },
+  {
+    number: 2,
+    name: "SWORDSMAN",
+    description1:
+      "Swordsman have travelled the world for centuries, gathering in places of power to study and hone their abilities. A true swordsman spends decades honing their craft until their sword is almost a third limb. They are some of the most deadly and feared warriors in the world.",
+    description2:
+      "Swordsman focus on close combat using blades. They can deal massive damage with their blades. They keep their enemies from attacking the weaker members of their party.",
+  },
+  {
+    number: 3,
+    name: "MAGE",
+    description1:
+      "Mages have focused their power into the summoning of demons and manipulation of the three realms; Heaven, Earth and Human. Their powers made the people considered evil spirits and they withdrew from the world for a time. Recently they finally revealed themselves again.",
+    description2:
+      "They focused on helping others and soon people began to see them as allies rather than enemies. A mage keeps his foes at a distance, using staves to launch magical attacks at their enemies. While weak physically their powers can cause massive damage.",
+  },
+  {
+    number: 4,
+    name: "NINJA",
+    description1:
+      "Ninjas are a mysterious organization that has existed hidden in the shadows for centuries. Long ago they served under the Zhou dynasty, striking down their foes. They are feared by the people who know of them, as one can never know when or where they may strike. Their hidden blades and clever poisons have killed hundreds of heroes and knights.",
+    description2:
+      "Ninjas focus on close combat against their foes. They are far more flexible than other warriors, using a variety of different weapons to cause a variety of effects.",
+  },
+  {
+    number: 5,
+    name: "PRIEST",
+    description1:
+      "During the warring states period the priests went deep into the Mon Vong swamp away from the fighting to focus on peace rather than war. For 50 years he meditated and studied the local plants and herbs. As time passed he unlocked the secrets of hundreds of potions, becoming an expert in pharmacology. They also learned many magical spells of great power.",
+    description2:
+      "The priest is a great healer, his powers keeping people alive in the most dangerous situations. When forced to fight the priest uses his staff to send magical attacks at his foes from a great distance.",
+  },
+  {
+    number: 6,
+    name: "TAOIST",
+    description1:
+      "There are many places of power across the world but the Taoists have gathered at the peak of Kun Lun. Here they can focus on the heavens, contacting demons and other celestial creatures, making deals with them to call on their abilities in the future. When not communing with the universe they study their skill with the blade.",
+    description2:
+      "Taoists are ranged warriors who use a staff to summon allies to fight for them. When not calling on allies they use a sword to attack their foes.",
+  },
+  {
+    number: 7,
+    name: "KNIGHT",
+    description1:
+      "Knight who follow the teaching of an unknown warrior in the past. His teachings are considered some of the most difficult and beautiful of all the warriors in the world. Warrior are part of a strong military tradition and they expect their warriors to train constantly to maintain peak physical health.",
+    description2:
+      "Knight are close quarter sword/Spear wielders focused on attacking many enemies.",
+  },
+];
+
 export default function Detailpage() {
+  const [currentTeam, setCurrentTeam] = useState(1);
+  const [flag, setFlag] = useState(true);
+
+  // useEffect(() => {
+  //   setInterval(() => {
+  //     setFlag(!flag);
+  //   }, 2000);
+  // });
   return (
     <>
       <Container maxWidth="xl">
@@ -83,8 +151,12 @@ export default function Detailpage() {
             {[...Array(7)].map((item, index) => (
               <Box
                 component="img"
-                src={`/characters/${index + 1}.png`}
-                sx={{ width: 132 }}
+                src={`/characters/${flag ? index + 1 : index + 8}.png`}
+                sx={{
+                  width: 135,
+                  cursor: "pointer",
+                }}
+                onClick={() => setCurrentTeam(index + 1)}
               />
             ))}
           </Stack>
@@ -102,10 +174,12 @@ export default function Detailpage() {
               >
                 <Box
                   component="img"
-                  src="/teams/team1-logo.png"
+                  src={`/teams/team${currentTeam}-logo.png`}
                   sx={{ width: 140 }}
                 />
-                <Typography variant="h1"> FIGHTER</Typography>
+                <Typography variant="h1">
+                  {TEAM_INFOS[currentTeam - 1].name}
+                </Typography>
               </Stack>
               <Typography>
                 In the distant past a warrior named Kuang Tu Nha helped the Chou
@@ -119,7 +193,7 @@ export default function Detailpage() {
               </Typography>
             </Stack>
             <Stack flex={1}>
-              <Box component="img" src="/teams/team1.png" />
+              <Box component="img" src={`/teams/team${currentTeam}.png`} />
             </Stack>
           </Stack>
         </Stack>
