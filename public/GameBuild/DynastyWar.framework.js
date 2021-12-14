2696075e2a7ed42c4b78782fa1ed9c8915164ced
Gold-Dev726 <goldendev726@gmail.com>
2021-12-14 09:15:59 -0500

    update

--- public/GameBuild/DynastyWar.framework.js
+++ public/GameBuild/DynastyWar.framework.js
@@ -1305,7 +1305,7 @@ function _emscripten_asm_const_id(code, a0) {
  return ASM_CONSTS[code](a0);
 }
 STATIC_BASE = GLOBAL_BASE;
-STATICTOP = STATIC_BASE + 2652848;
+STATICTOP = STATIC_BASE + 2653440;
 __ATINIT__.push({
  func: (function() {
   __GLOBAL__sub_I_AccessibilityScriptingClasses_cpp();
@@ -3359,7 +3359,7 @@ __ATINIT__.push({
   ___emscripten_environ_constructor();
  })
 });
-var STATIC_BUMP = 2652848;
+var STATIC_BUMP = 2653440;
 Module["STATIC_BASE"] = STATIC_BASE;
 Module["STATIC_BUMP"] = STATIC_BUMP;
 var tempDoublePtr = STATICTOP;
